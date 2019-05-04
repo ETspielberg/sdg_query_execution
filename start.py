9d7cce8b950777d93cbe9b8d61a674e2c1cbfb2d
Eike Spielberg <Eike.Spielberg@uni-due.de>
2019-05-04 06:20:26 +0200

    returning 404 when no keywords are found

--- start.py
+++ start.py
@@ -217,14 +217,17 @@ def download_sample_eids(query_id):
 # retrieve the list of keywords for the search
 @app.route("/keywords/<query_id>")
 def get_keywords(query_id):
-    result = es.search(index=query_id, doc_type='all_data',
-                       filter_path=["hits.hits._source.scopus_abtract_retrieval.authkeywords", "hits.hits._id"])
-    keyword_list = []
-    for hit in result["hits"]["hits"]:
-        keyword_list.extend(hit["_source"]["scopus_abtract_retrieval"]["authkeywords"])
-    dictionary = utils.wordlist_to_freq_dict(keyword_list)
-    sorted_dict = utils.sort_freq_dict(dictionary)
-    return json.dumps([ob.__dict__ for ob in sorted_dict])
+    try:
+        result = es.search(index=query_id, doc_type='all_data',
+                           filter_path=["hits.hits._source.scopus_abtract_retrieval.authkeywords", "hits.hits._id"])
+        keyword_list = []
+        for hit in result["hits"]["hits"]:
+            keyword_list.extend(hit["_source"]["scopus_abtract_retrieval"]["authkeywords"])
+        dictionary = utils.wordlist_to_freq_dict(keyword_list)
+        sorted_dict = utils.sort_freq_dict(dictionary)
+        return json.dumps([ob.__dict__ for ob in sorted_dict])
+    except Exception as e:
+        return Response('no keywords ', status=404)
 
 
 # returns true if the eids_list.txt file is present for the given project
