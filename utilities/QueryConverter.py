3d01696853c9d3e52e3288ca46fc1446b2570af3
Eike Spielberg <Eike.Spielberg@uni-due.de>
2020-07-09 14:26:28 +0200

    cleaning output fields.

--- utilities/QueryConverter.py
+++ utilities/QueryConverter.py
@@ -26,25 +26,22 @@ class QueryConverter:
 
     def calculate_scopus_queries(self):
         scopus_queries = ScopusQueries()
-        overall_filter_string = get_filter_string(self._query.query_definitions.query_filters)
+        overall_filter_string = clean_up_line(get_filter_string(self._query.query_definitions.query_filters))
         query_string = overall_filter_string
         overall_query_string = overall_filter_string
         query_length = query_string.__len__()
         for query_definition in self._query.query_definitions.query_definition:
             for query_line in query_definition.query_lines:
+                query_term = ''
                 if query_line.query_line is not '':
 
                     # generate query term for indiviudal query line
-                    query_term = query_line.field + '(' + re.sub(' +', ' ',
-                                                                 query_line.query_line.replace('\n', '').replace('\t', '')) + ')'
+                    query_term = query_line.field + '(' + clean_up_line(query_line.query_line) + ')'
 
                     # add query term to overall query string. if it is the first entry, connect by AND and open bracket,
                     # else add it by OR
                     if overall_query_string is overall_filter_string:
-                        if overall_query_string == '':
-                            overall_query_string = '(' + query_term
-                        else:
-                            overall_query_string += ' AND (' + query_term
+                        overall_query_string = '(' + query_term
                     else:
                         overall_query_string += ' OR ' + query_term
 
@@ -55,28 +52,25 @@ class QueryConverter:
                         # add query term to query string. if it is the first entry, connect by AND and open bracket,
                         # else add it by OR
                         if query_string is overall_filter_string:
-                            query_string += ' AND (' + query_term
+                            query_string += '(' + query_term
                         else:
                             query_string += ' OR ' + query_term
                     else:
                         scopus_queries.add_search_string(query_string + ')')
-                        query_string = overall_filter_string + ' AND (' + query_term
-        scopus_queries.add_search_string(re.sub(' +', ' ', query_string + ')')
-                                         .replace('\n', '')
-                                         .replace('\t', '')
-                                         .replace('\u201c', '"')
-                                         .replace('\u201d', '"')
-                                         .replace('\u2018', '"')
-                                         .replace('\u2019', '"'))
-        scopus_queries.overall = re.sub(' +', ' ', overall_query_string + ')')\
-            .replace('\n', '')\
-            .replace('\t', '') \
-            .replace('\u201c', '"')\
-            .replace('\u201d', '"')\
-            .replace('\u2018', '"')\
-            .replace('\u2019', '"')
+                        query_string = overall_filter_string + '(' + query_term
+                if query_term is not '':
+                    scopus_queries.add_search_string(query_string + ')')
+                    query_string = overall_filter_string + '(' + query_term
+        scopus_queries.add_search_string(query_string + ')')
+        scopus_queries.overall = overall_query_string + ')'
         self._scopus_queries = scopus_queries
         return self._scopus_queries
+
+
+def clean_up_line(line):
+    new_line = re.sub(' +', ' ', line).replace('\n', '').replace('\t', '').replace('\u201c', '"')\
+        .replace('\u201d', '"').replace('\u2018', '"').replace('\u2019', '"')
+    return new_line
 
 
 def get_filter_string(query_filters):
@@ -86,18 +80,16 @@ def get_filter_string(query_filters):
             string = '(' + query_filters.timerange.field + ' AFT ' + query_filters.timerange.start
             if query_filters.timerange.end is not '':
                 string = string + ' AND ' + query_filters.timerange.field + ' BEF ' + query_filters.timerange.end
-            string = string + ')'
+            string = string + ') AND '
     for query_filter in query_filters.query_filters:
-        if string != '':
-            string += ' AND '
         if query_filter.filter_type is 'subjectarea':
             string += '('
             for i, subjects in enumerate(query_filter.filter_term.split(' OR ')):
                 if i > 0:
                     string += ' OR '
                 string += ' LIMIT-TO(SUBJAREA, \'' + query_filter.filter_term + '\')'
-            string += ')'
+            string += ') AND '
         elif query_filter.filter_type:
-            string += '(' + query_filter.filter_field + '(' + query_filter.filter_term + '))'
+            string += '(' + query_filter.filter_field + '(' + query_filter.filter_term + ')) AND '
             string = string.replace(' OR ', ') OR ' + query_filter.filter_field + '(')
     return string
